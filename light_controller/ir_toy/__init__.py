427acb3da56dd381e2e336aee7a682bb9784a38b
Andrew Milton <amilton@assurance.com>
2021-03-12 12:04:34 -0800

    Revert "Initial Import"
    This reverts commit 6c1cff94f42458589962e8c1ee6b175f68fe26c4.

--- light_controller/ir_toy/__init__.py
+++
@@ -1,4 +0,0 @@
-# -*- coding: utf-8 -*-
-__author__ = 'akm'
-
-from IR_Toy import IR_Toy
--- light_controller/ir_toy/ir_toy_settings.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-from collections import namedtuple
-__author__ = 'akm'
-
-IR_ToySettings = namedtuple('IR_ToySettings', ('pr2', 'dutyCycle', 'prescaler', 'TX_Timer', 'frequency'))
